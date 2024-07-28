3ba02cc703ed1c65a16b602ac1d72a657fb64a8d
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2024-07-28 15:43:25 +0530

    Form without courses

---
+++ src/app/contact-us/page.jsx
@@ -0,0 +1,69 @@
+'use client'
+
+import Navbar from "@/components/NavBar";
+import { useState } from "react";
+
+
+
+
+
+const ContactUs = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => setDarkMode((prev) => !prev);
+
+
+  return(
+    <div className={`${darkMode ? 'dark' : ''}`}>
+        <section className="bg-white dark:bg-gray-900 ">
+          
+          {/*Navbar Start  */}
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} ></Navbar>
+          {/*Navbar end  */}
+
+          {/* Form Start */}
+
+          <main className="min-h-screen flex justify-center items-center"> 
+          <form className="max-w-sm mx-auto">
+            <div className="mb-5">
+            <div className="mb-5">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+              <input type="text" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            </div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+            </div>
+            
+            <div className="mb-5">
+              <label htmlFor="phoneNo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Phone Number</label>
+              <input type="tel" id="phoneNo" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" pattern="[0-9]{10}" title="Please enter a 10 digit number" required />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Any Other Questions ?</label>
+              <input type="text" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            </div>
+            <div className="flex items-start mb-5">
+              <div className="flex items-center h-5">
+                <input id="terms" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"  required />
+              </div>
+              <label htmlFor="terms" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+          </form>
+          </main>
+
+
+          {/* Form End */}
+
+
+          
+          
+         </section>
+    </div>
+  )
+}
+
+
+export default ContactUs;
+
+
