412bf42d9fb5654bd5b432f5cbb9ad8fce5e85b8
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2024-07-29 01:25:56 +0530

    Contact Logo

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -106,6 +106,7 @@ const ContactUs = () => {
           />
           <main className="min-h-screen flex flex-col justify-center items-center">
             <form onSubmit={handleSubmit} className="max-w-sm mx-auto mb-5">
+              <div className="w-full flex justify-center"><img  src="/images/logo.png" className="h-36"></img></div>
               <div className="mb-5">
                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
                 <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
